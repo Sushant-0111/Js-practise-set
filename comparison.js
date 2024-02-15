beb30916e468ae3403d8c2a54fd129dd49f75ef9
Sushant-0111 <84455858+Sushant-0111@users.noreply.github.com>
2024-02-15 22:11:32 +0545

    summary data type

---
+++ comparison.js
@@ -0,0 +1,20 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>=1);
+
+
+const id = Symbol("12")
+console.log(typeof id);
+
+
+// Datatype 
+
+//   typeof number = number 
+//   typeof string = string
+//   typeof boolean = boolean 
+//   typeof undefined= undefined 
+//   typeof null = obj
+
+
